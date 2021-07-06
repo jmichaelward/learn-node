08bed1a4329ac8298d88dc9a6dcdd4e2d29ad969
Jeremy Ward <jeremy@jmichaelward.com>
2021-07-05 19:33:19 -0500

    Add an error route to demonstrate error handling in Express.

--- fibonacci/routes/index.js
+++ fibonacci/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Example route to demonstrate error handling */
+router.get('/error', function(request, response, next) {
+  next({
+    status: 404,
+    message: 'Fake error',
+  })
+});
+
 module.exports = router;
